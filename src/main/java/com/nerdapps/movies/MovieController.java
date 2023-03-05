192d67dc3d1faa5df0d68d8486167b9431cec869
Felipe Inostroza <sinfonico7@gmail.com>
2023-03-05 11:22:51 -0300

    My first push

---
+++ src/main/java/com/nerdapps/movies/MovieController.java
@@ -0,0 +1,30 @@
+package com.nerdapps.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> allMovies(){
+        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<>(movieService.getSingleMovieById(imdbId),HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/nerdapps/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdapps.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
